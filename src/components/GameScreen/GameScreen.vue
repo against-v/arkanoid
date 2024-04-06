a5c005287248c18bfddfa05d76830e113e200cf4
a.azdakhov <a.azdakhov@sociallinks.io>
2024-04-06 14:27:33 +0200

    Configures app

---
+++ src/components/GameScreen/GameScreen.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div :class="$style.container">GameScreen</div>
+</template>
+
+<style module>
+.container {
+  color: white;
+  background-color: grey;
+  height: 100%;
+}
+</style>
