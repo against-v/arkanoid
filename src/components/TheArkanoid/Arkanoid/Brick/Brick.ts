52d08fa61582b8ca309a85fd6c9dce5642ffffa3
a.azdakhov <a.azdakhov@sociallinks.io>
2024-04-07 21:52:16 +0200

    adds bricks

---
+++ src/components/TheArkanoid/Arkanoid/Brick/Brick.ts
@@ -0,0 +1,16 @@
+import { Graphics, type PointData } from "pixi.js";
+
+export class Brick extends Graphics {
+  objWidth: number;
+  objHeight: number;
+  constructor(position: PointData, width: number, height: number, color: number, points: number) {
+    super();
+    this.objWidth = width;
+    this.objHeight = height;
+    this.rect(0, 0, this.objWidth, this.objHeight);
+    this.fill(color);
+    this.stroke({ width: 1, color: 0x000000 });
+    this.x = position.x;
+    this.y = position.y;
+  }
+}
---
+++ src/components/TheArkanoid/Arkanoid/BricksCollection/index.ts
@@ -0,0 +1,1 @@
+export { BricksCollection } from "./BricksCollection";
