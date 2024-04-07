7d3fecd41726b9fe0fd1bc3c756477241a591da7
a.azdakhov <a.azdakhov@sociallinks.io>
2024-04-07 20:17:59 +0200

    Refactors

--- src/components/TheArkanoid/Arkanoid/BallInteraction/BallInteraction.ts
+++ src/components/TheArkanoid/Arkanoid/BallInteraction/BallInteraction.ts
@@ -1,20 +1,9 @@
-import type { Application, Container } from "pixi.js";
+import type { Application } from "pixi.js";
 import type { Ball } from "@/components/TheArkanoid/Arkanoid/Ball";
 import { Platform } from "@/components/TheArkanoid/Arkanoid/Platform";
+import { testForAABB } from "@/utils/testForAABB";
 
 const SPEED = 10;
-
-const testForAABB = (object1: Container, object2: Container) => {
-  const bounds1 = object1.getBounds();
-  const bounds2 = object2.getBounds();
-
-  return (
-    bounds1.x < bounds2.x + bounds2.width &&
-    bounds1.x + bounds1.width > bounds2.x &&
-    bounds1.y < bounds2.y + bounds2.height &&
-    bounds1.y + bounds1.height > bounds2.y
-  );
-};
 
 export class BallInteraction {
   app: Application;
